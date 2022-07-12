cd3b468cf111c3569fdc3073426f7b511fc513ec
tejaskhartude <tejaskhartude@gmail.com>
2022-07-12 20:37:44 +0530

    sINGLETON Added

---
+++ src/main/kotlin/classes/Objects.kt
@@ -0,0 +1,18 @@
+package classes
+
+object Global{
+
+    val PI = 3.14
+}
+
+fun main(){
+
+    val localObject = object {
+        val PI = 3.14159
+    }
+
+    println(localObject.PI)
+    println(Global.PI)
+
+
+}
