ef952800e0fb7e47080e8931baeb66dcbe9aa61a
tejaskhartude <tejaskhartude@gmail.com>
2022-07-14 13:47:03 +0530

    Added Higher Order Functions

---
+++ src/main/kotlin/functional/GettingFunctional.kt
@@ -0,0 +1,14 @@
+package functional
+
+
+fun operation(x:Int,y:Int,op:(Int,Int)->Int):Int{
+  return op(x,y)
+}
+
+fun sum(x:Int,y:Int) = x+y
+
+fun main(){
+
+    println(operation(1,2,::sum))
+
+}
